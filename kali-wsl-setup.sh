861ceeefd89fdc1d030415a4cf4dd931e949efe8
Suljov <albin.suljov@hotmail.com>
2023-11-22 12:06:47 +0100

    Update kali-wsl-setup.sh

--- kali-wsl-setup.sh
+++ kali-wsl-setup.sh
@@ -100,8 +100,7 @@ mv pspy32 pspy64 system/linux/
 
 sudo apt install --only-upgrade firefox-esr
 
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install sublime-text
