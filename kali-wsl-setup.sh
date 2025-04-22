1bba949e68a07e634eac05ac7070ffa7066f8f25
Suljov <albin.suljov@hotmail.com>
2025-04-22 22:20:45 +0200

    Update kali-wsl-setup.sh

--- kali-wsl-setup.sh
+++ kali-wsl-setup.sh
@@ -121,7 +121,7 @@ cd ..
 sudo wget https://raw.githubusercontent.com/payloadbox/xss-payload-list/master/Intruder/xss-payload-list.txt -O /usr/share/wordlists/xss-payload-list.txt
 sudo wget https://raw.githubusercontent.com/payloadbox/ssti-payloads/master/Intruder/ssti-payloads.txt -O /usr/share/wordlists/ssti-payloads.txt
 sudo wget https://raw.githubusercontent.com/payloadbox/ssti-payloads/master/Intruder/ssti-urlencoded-payloads.txt -O /usr/share/wordlists/ssti-urlencoded-payloads.txt
-sudo wget https://raw.githubusercontent.com/suljov/One-API-to-rule-them-all/main/API-RuleThemAll.txt -O /usr/share/wordlists/API-RuleThemAll.txt
+sudo wget https://raw.githubusercontent.com/suljov/One-API-to-rule-them-all/refs/heads/main/API-RuleThemAll.txt -O /usr/share/wordlists/API-RuleThemAll.txt
 
 
 wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas_linux_amd64 
