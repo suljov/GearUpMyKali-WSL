34d91349e27e3504c05a36f67e5501204f21c47c
Suljov <albin.suljov@hotmail.com>
2025-10-04 22:00:57 +0200

    Update GearUpMyKali-WSL.sh

--- GearUpMyKali-WSL.sh
+++ GearUpMyKali-WSL.sh
@@ -327,6 +327,7 @@ repo_urls=(
     "https://github.com/carlospolop/cloudpeass"
     "https://github.com/cobbr/SharpSploit"
     "https://github.com/olafhartong/Invoke-Phant0m"
+    "https://github.com/ra1nb0rn/search_vulns"
 )
 
 # Loop through the repository URLs and clone each one into the current directory
@@ -338,7 +339,7 @@ done
 
 mv Ghostpack-CompiledBinaries Inveigh Windows-Exploit-Suggester windapsearch Rubeus PrivescCheck mimikatz nc.exe bloodyAD krbrelayx ntlm_theft PetitPotam CVE-2023-2255 SharpUp Seatbelt impacket Invoke-Phant0m SharpSploit SharpCollection PrecompiledBinaries BloodHound.py adPEAS donut BloodHound powersploit windowsprivchecker system/windows/
 mv linux-exploit-suggester linux-exploit-suggester-2 LinEnum linuxprivchecker system/linux/
-mv Pentest-Cheatsheets misc/
+mv Pentest-Cheatsheets search_vulns misc/
 mv TokenTacticsV2 AzSubEnum Oh365UserFinder o365spray o365enum GraphRunner basicblobfinder Graphpython BARK cloud/Azure
 mv cloudpeass cloud/Misc
 mv SSLPinDetect mobile
@@ -410,6 +411,7 @@ go install -v github.com/projectdiscovery/urlfinder/cmd/urlfinder@latest
 go install github.com/tomnomnom/gf@latest
 go install github.com/tomnomnom/qsreplace@latest
 go install github.com/projectdiscovery/katana/cmd/katana@latest
+go install github.com/projectdiscovery/cvemap/cmd/vulnx@latest
 
 git clone https://github.com/devanshbatham/paramspider
 cd paramspider
